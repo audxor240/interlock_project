ba048f60825a4d44e10f9a910b1528894bcab76d
audxor240 <audxor240@naver.comn>
2023-07-04 12:53:25 +0900

    Initial commit

---
+++ src/test/java/com/example/interlock_project/InterlockProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.interlock_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterlockProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
